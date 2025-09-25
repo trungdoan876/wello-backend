1f3bd5c3a08b892b137bf9bf10e302e2cbb1a60a
trungdoan876 <trungdoan876@gmail.com>
2025-09-25 15:54:40 +0700

    init

---
+++ src/main/java/com/wello/wellobackend/WelloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wello.wellobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelloBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelloBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wello/wellobackend/WelloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wello.wellobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelloBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
